8f9d944d64d13a074d980cbde2d5a0788cf9ca18
Malik <malikiy8@gmail.com>
2021-10-04 05:54:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,8 @@
  *
  * @param
  */
-if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
+if (process.env.NODE_ENV !== 'production') { require('dotenv').config({path: '.env'}) }
+require('dotenv').config({ path: 'ENV_FILENAME' });
 
 module.exports = {
     dbUrl : process.env.DATABASE_URL,
